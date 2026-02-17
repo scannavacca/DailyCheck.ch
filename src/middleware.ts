1438866dea9f98ba803bfdc1fc451a72463ee8d5
scannavacca <scannavacca88@gmail.com>
2026-02-17 19:29:44 +0100

    Allow PWA assets and manifest

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,12 @@ export function middleware(request: NextRequest) {
     pathname.startsWith("/_next") ||
     pathname.startsWith("/_expo") ||
     pathname.startsWith("/assets") ||
+    pathname.startsWith("/image") ||
     pathname === "/favicon.ico" ||
+    pathname === "/sw.js" ||
+    pathname === "/manifest.webmanifest" ||
     pathname === "/app-index.html" ||
+    pathname === "/Logo.png" ||
     pathname.endsWith(".html")
   ) {
     return NextResponse.next();
